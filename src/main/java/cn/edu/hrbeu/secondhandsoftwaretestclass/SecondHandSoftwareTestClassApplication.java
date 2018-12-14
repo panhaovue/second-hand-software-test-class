c0e6d81f81e96935a786d81cfaa7aaeb935c3910
guokun <365690468@qq.com>
2018-12-14 13:49:04 +0800

    Initial commit

---
+++ src/main/java/cn/edu/hrbeu/secondhandsoftwaretestclass/SecondHandSoftwareTestClassApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.hrbeu.secondhandsoftwaretestclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandSoftwareTestClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandSoftwareTestClassApplication.class, args);
+    }
+
+}
+
